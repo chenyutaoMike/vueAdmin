557a358f51933b833c770fcca552adc6e54d3d75
Mike <1032098274@qq.com>
2020-07-01 21:35:14 +0800

    构建项目，按需引入elemeui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Button, Select } from 'element-ui';
+
+Vue.component(Button.name, Button);
+Vue.component(Select.name, Select);
 
 Vue.config.productionTip = false;
 
